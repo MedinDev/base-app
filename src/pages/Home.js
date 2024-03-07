11e5c594b86b73c0f5f6327cdf6a357d243bb021
Medtex <medtexprog@gmail.com>
2024-03-07 23:52:41 +0800

    day006

---
+++ src/pages/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import MainLayout from "../layout/MainLayout";
+
+function Home(props) {
+    return (
+        <MainLayout>
+        <div><h2>Hik</h2></div>
+        </MainLayout>
+    );
+}
+
+export default Home;

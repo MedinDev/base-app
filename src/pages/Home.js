1c79e0d0f1998bf28eaa54fb9d23ddfb4cd2a7e2
Medtex <medtexprog@gmail.com>
2024-03-09 05:04:49 +0800

    day008

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import MainLayout from "../layout/MainLayout";
 
+
 function Home(props) {
     return (
         <MainLayout>
-        <div><h2>Hik</h2></div>
+            Home
         </MainLayout>
     );
 }
